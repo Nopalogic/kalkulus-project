8773d3d3bb1880b32ea61062108c2940574cfaca
Nopalogic <naufaladhi02@gmail.com>
2022-12-22 20:57:37 +0700

    modification

--- src/pages/fungsi/index.jsx
+++ src/pages/fungsi/index.jsx
@@ -76,17 +76,19 @@ const Fungsi = () => {
 										Penyajian fungsi dengan menggunakan rumus matematis, misalnya luas lingkaran adalah <InlineMath math="L=\pi r^2" />. Disini domainnya adalah jari-jari {'(r)'} dan rangenya adalah luas {'(L)'}. <br />
 										Contoh: <br />
 										<div className="container">
-											Carilah daerah defini dan daerah nilai dar fungsi <InlineMath math="g(x)=\sqrt{9-x^2}" />
+											Carilah daerah definisi dan daerah nilai dari fungsi <InlineMath math="g(x)=\sqrt{9-x^2}" />
 											<br />
-											Dalam hal ini kita harus membatasi x sehingga <InlineMath math="g(x) = 9-x^2 \ge 0" />, dengan tujuan menghindari nilai tak riil.
-											<br />
-											Menentukan domain g dapat mengguanakan cara berikut: <br />
-											<InlineMath math="9-x^2 \ge 0" />, maka <InlineMath math="9 \ge x^2" /> atau <InlineMath math="x^2 \le 9" />, sehingga <InlineMath math="-3 \le x \le 3" /> <br />
-											Jadi domain g adalah <InlineMath math="D_g = \set{x \in R | -3 \le x \le 3}" /> <br />
-											Sedangkan untuk mencari range, dapat dikonstruksi dari domain sehingga mendapat batas nilai untuk fungsi <InlineMath math="g(x)=\sqrt{9-x^2}" /> yang dapat dilakukan dengan cara berikut: <br />
-											<InlineMath math="-3 \le x \le 3" />, maka <InlineMath math="0 \le x^2 \le 9" />, maka <InlineMath math="-9 \le -x^2 \le 0" />, sehingga <InlineMath math="0 \le 9-x^2 \le 9" />, maka{' '}
-											<InlineMath math="0 \le\sqrt{9-x^2}\le 3" /> <br />
-											Jadi range g adalah <InlineMath math="R_g=\set{g(x) \in R | 0 \le g(x) \le 3}" />
+											<p className='mt-2'>
+												Dalam hal ini kita harus membatasi x sehingga <InlineMath math="g(x) = 9-x^2 \ge 0" />, dengan tujuan menghindari nilai tak riil.
+												<br />
+												Menentukan domain g dapat mengguanakan cara berikut: <br />
+												<InlineMath math="9-x^2 \ge 0" />, maka <InlineMath math="9 \ge x^2" /> atau <InlineMath math="x^2 \le 9" />, sehingga <InlineMath math="-3 \le x \le 3" /> <br />
+												Jadi domain g adalah <InlineMath math="D_g = \set{x \in R | -3 \le x \le 3}" /> <br />
+												Sedangkan untuk mencari range, dapat dikonstruksi dari domain sehingga mendapat batas nilai untuk fungsi <InlineMath math="g(x)=\sqrt{9-x^2}" /> yang dapat dilakukan dengan cara berikut: <br />
+												<InlineMath math="-3 \le x \le 3" />, maka <InlineMath math="0 \le x^2 \le 9" />, maka <InlineMath math="-9 \le -x^2 \le 0" />, sehingga <InlineMath math="0 \le 9-x^2 \le 9" />, maka{' '}
+												<InlineMath math="0 \le\sqrt{9-x^2}\le 3" /> <br />
+												Jadi range g adalah <InlineMath math="R_g=\set{g(x) \in R | 0 \le g(x) \le 3}" />
+											</p>
 										</div>
 									</p>
 								</section>
@@ -121,7 +123,8 @@ const Fungsi = () => {
 										<div className="container">
 											fungsi f dan g dikatakan sama {'(ditulis f=g),'} jika fungsi tersebut <InlineMath math="D_f=D_g" /> dan <InlineMath math="f(x)=g(x)" />, untuk setiap <InlineMath math="x \in D" /> <br />
 											Contoh: <br />
-											Fungsi <InlineMath math='f(x) = \cos^2 x' /> dan <InlineMath math='g(x)=1-\sin^2x' /> adalah sama, karena <InlineMath math='D_f = D_g =R' /> dan <InlineMath math='f(x) = g(x)' />, untuk setiap <InlineMath math="x \in R" />
+											Fungsi <InlineMath math="f(x) = \cos^2 x" /> dan <InlineMath math="g(x)=1-\sin^2x" /> adalah sama, karena <InlineMath math="D_f = D_g =R" /> dan <InlineMath math="f(x) = g(x)" />, untuk setiap{' '}
+											<InlineMath math="x \in R" />
 										</div>
 									</p>
 								</section>
